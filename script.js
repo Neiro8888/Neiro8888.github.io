ec034a8a55864772a1469f6feb6c4610bef60238
Neiro <157352656+Neiro8888@users.noreply.github.com>
2026-03-28 22:41:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Плавная прокрутка по якорям
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  });
+});
